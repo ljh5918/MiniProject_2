37a36c6ef782fd4ea2cd011d45cb9700dad0b670
ljh5918 <akwogur123@naver.com>
2025-12-15 15:46:49 +0900

    회원가입 이메일, 닉네임 중복 방지 코드 추가

--- src/main/java/com/mycom/myapp/user/repository/UserRepository.java
+++ src/main/java/com/mycom/myapp/user/repository/UserRepository.java
@@ -8,6 +8,9 @@ import java.util.Optional; // import 추가
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     
-    // ⭐ [추가] 로그인 시 이메일로 사용자 전체 정보 조회
+    // ⭐ 로그인 시 이메일로 사용자 전체 정보 조회
     Optional<User> findByEmail(String email); 
+    
+    // ⭐ 닉네임 중복 체크
+    boolean existsByNickname(String nickname);
 }
