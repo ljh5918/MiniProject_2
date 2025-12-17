b9d725cda1504e46d1741a1042c2925191c11ae6
ljh5918 <akwogur123@naver.com>
2025-12-17 17:05:28 +0900

    코드 리팩토링

--- src/main/java/com/mycom/myapp/favorite/controller/FavoriteController.java
+++ src/main/java/com/mycom/myapp/favorite/controller/FavoriteController.java
@@ -51,21 +51,6 @@ public class FavoriteController {
         return ResponseEntity.ok("success");
     }
     
-
-
-
-//    @GetMapping("/list")
-//    public ResponseEntity<?> getFavorites(HttpServletRequest request) {
-//
-//        String token = extractToken(request);
-//        if (token == null || !jwtTokenProvider.validateToken(token)) {
-//            return ResponseEntity.status(401).body("Unauthorized");
-//        }
-//
-//        String email = jwtTokenProvider.getAuthentication(token).getName();
-//
-//        return ResponseEntity.ok(favoriteService.getFavorites(email));
-//    }
     
     @GetMapping("/list")
     public ResponseEntity<?> getFavorites(HttpServletRequest request) {
